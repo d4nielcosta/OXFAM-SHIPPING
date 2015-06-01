805551b68bc612b827bd25630adc4812cb1fcc6a
Daniel Costa <danielcosta@gmx.com>
2015-06-01 16:45:57 +0100

    Seperate Emails in different file

--- general/views.py
+++ general/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from datetime import date
 from models import *
 import calendar
-from django.core.mail import send_mail
-from django.conf import settings
+from email_list import *
+
 
 # Create your views here.
 
@@ -116,47 +116,8 @@ def index(request):
             reference2_email=request.session['sr_email'])
 
         volapp.save()
-
-
-        # Send Email
-        subject = 'OXFAM - Thank you for applying to volunteer with us.'
-        from_email = settings.EMAIL_HOST_USER
-        to_list = [volapp.email]
-        message = \
-            "Hi. Thank you for applying to volunteer with us. We will get in touch with you as soon as possible. \n " \
-            "In the meanwhile, here's a copy of your application: \n " \
-            "YOUR DETAILS \n" \
-            "       Forename: " + volapp.forename + "\n" \
-            "       Surname: " + volapp.surname + "\n" \
-            "       Email: " + volapp.email + "\n" \
-            "       Primary Phone: " + volapp.primary_phone + "\n" \
-            "       Secondary Phone: " + volapp.secondary_phone + "\n" \
-            "       Date of Birth: " + volapp.dob.strftime('%d/%m/%Y') + "\n" \
-            "       Address \n" + volapp.address + "\n \n "\
-            "PERMISSION TO WORK \n" \
-            "       In the UK (goverment) " + str(volapp.permission_to_work) + "\n" \
-            "       Parental Permission (if minor) " + str(volapp.parental_permission) + "\n" \
-            "\n" \
-            "EMERGENCY CONTACT" + "\n" \
-            "       Name: " + volapp.emergency_contact_forename + volapp.emergency_contact_surname + "\n" \
-            "       Phone Number: " + volapp.emergency_contact_phone + "\n" \
-            "\n" \
-            "REFERENCES" + "\n" \
-            "       Forename: " + volapp.reference1_forename + "\n" \
-            "       Surname: " + volapp.reference1_surname + "\n" \
-            "       Phone: " + volapp.reference1_primary_phone + "\n" \
-            "       Email: " + volapp.reference1_email + "\n" \
-            "\n" \
-            "       Forename: " + volapp.reference2_forename + "\n" \
-            "       Surname: " + volapp.reference2_surname + "\n" \
-            "       Phone: " + volapp.reference2_primary_phone + "\n" \
-            "       Email: " + volapp.reference2_email + "\n" \
-            "\n \n"\
-            "Best Regards \n The Oxfam Team"
-
-        send_mail(subject, message, from_email, to_list, fail_silently=False)
-
-        print "Volunteer Application saved"
+        send_application_email(volapp)
+        send_email_confirmation(volapp)
 
         request.session.flush()
         request.session['rgstr_step'] = 0
